cff182ee7c701bb0bd5b13cb4e13e7005b94b71a
nabin <xattrinabin122@g,ail.com>
2024-05-11 09:10:02 +0545

    business dashboard complete

--- 17miniserverclient/client/src/hooks/useRegister.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import registerContext from "../contexts/registerContext";
-function useRegister (){
-    const register = useContext(registerContext);
-    return register;
-}
-
-export default useRegister;
